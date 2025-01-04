e9499789e687a19eccca052fdfc226c36d4876ae
Anil Ghuge <anil.ghuge@vinsys.com>
2025-01-04 09:02:19 +0530

    ArbitaryInjection

---
+++ Proj04ArbitaryInjection/ArbitaryInjection.md
@@ -0,0 +1,6 @@
+# Arbitary Method Injection:
+
+## Key Points:
+- This Method is same as setter method but we can take our choice name for it.
+- aribitary name means any name. so aribitary method means method with any name.
+-  In this Injection,the IOC container calls aribitary methods to inject depedent spring bean class object to target bean class object.
---
+++ Proj04ArbitaryInjection/src/com/example/client/ArbitaryInjection.java
@@ -0,0 +1,21 @@
+package com.example.client;
+
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.example.sbean.WishMessageGenerator;
+
+public class ArbitaryInjection {
+
+	public static void main(String[] args) {
+		FileSystemXmlApplicationContext context=new FileSystemXmlApplicationContext("src/com/example/cfg/applicationContext.xml");
+		Object bean = context.getBean("wmg");
+		WishMessageGenerator obj=(WishMessageGenerator)bean;
+		String generatorMessage = obj.getGeneratorMessage("Anil");
+		System.out.println("Result :"+generatorMessage);
+		
+		Object bean1 = context.getBean("wmg");
+		System.out.println(bean.hashCode()+" "+bean1.hashCode());
+		System.out.println("obj==obj1 "+(bean==bean1));
+		context.close();
+	}
+}
